eab55d102eefb95372d4e5dd2d057998055386d0
IT Recipe <itrecipe@naver.com>
2023-08-13 16:27:07 +0900

    java_exam
    23.8.13

--- Chapter1/src/ch12/LogicalTest1.java
+++ Chapter1/src/ch12/LogicalTest1.java
@@ -9,14 +9,14 @@ public class LogicalTest1 {
 		
 		boolean value = ((num1 = num1 + 10) < 10) && ((i = i + 2) < 10);
 		//boolean value1 = ((num1 = num1 + 10) < 10) || ((i = i + 2) < 10);
+		
 		/* - 앞의 항이 이미 false이므로 컴퓨터는 더이상 뒤의 항은 연산하지 않음
 		   - 그래서 num1값은 변동되지만 i값은 그대로 출력됨 &&, || 조건 둘다 동일함
 		*/
-		
+
 		System.out.println(value);
 		//System.out.println(value1);
 		System.out.println(num1);
 		System.out.println(i);
 	}
-
 }
